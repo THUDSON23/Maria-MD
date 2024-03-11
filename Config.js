b428ae2d867e35e3eb9791adb05f54c4600fd8cb
THUDSON23 <138740637+THUDSON23@users.noreply.github.com>
2024-03-11 21:53:54 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,27 +2,27 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['237680255347']
+global.ownername = "HUDSON 𓆪_✰"//owner name
 global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
+global.socialm = "GitHub: THUDSON23"
 global.location = "Indian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'BOTSON-MD' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
-global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.stickername = 'ʙᴏᴛꜱᴏɴ ᴍᴅ'
+global.packname = 'Sʙᴏᴛꜱᴏɴ ᴍᴅ'
+global.author = 'BOTSON'
 //console view/theme
-global.themeemoji = '🧩'
+global.themeemoji = '😸'
 global.wm = "Ayush botz inc."
 
 //theme link
 global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
 
 //custom prefix
-global.prefa = ['.']
+global.prefa = ['+']
 
 //false=disable and true=enable
 global.welcome = false //auto welcome
@@ -32,17 +32,17 @@ global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti212 = true //auto block +212
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 
 
 //reply messages
 global.mess = {
     done: '*here you go!* \n\n*🎀 Maria Bot 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/AYUSH-PANDEY023/Maria-MD\n',
-    prem: '*This feature can be used by premium user only*',
+    prem: '*This feature can be used by Hudson only*',
     admin: '*This feature can be used by admin only*',
-    botAdmin: '*This feature can only be used when the bot is a group admin* ',
-    owner: '*This feature can be used by owner only*',
+    botAdmin: '*This feature can only be used when the botson is a group admin* ',
+    owner: '*This feature can be used by Hudson only*',
     group: '*This feature is only for groups*',
     private: '*This feature is only for private chats*',
     wait: '*In process...* ',    
